10cc5bc4ccd16cfc3156f3115e4c6ca5fdb046fc
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-06-21 16:09:17 +0200

    views

---
+++ Client/src/main/java/ch/tbz/client/frontend/controller/_prefaps/SettingsController.java
@@ -0,0 +1,9 @@
+package ch.tbz.client.frontend.controller._prefaps;
+
+import javafx.event.ActionEvent;
+
+public class SettingsController {
+
+    public void toggleDarkMode(ActionEvent actionEvent) {
+    }
+}
